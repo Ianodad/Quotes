92c6cf6fc0aaac696b8854c51661645cd54d7f83
Ian Odhiambo <you@example.com>
2018-08-10 18:43:37 +0300

    created quote c, quote-details and quote class

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote} from '../quote';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+
+  quotes = [
+    new Quote(1, 'According to your faith be it done to you', 'Jesus', 'Matthew', new Date(-40, 3, 14)),
+    new Quote(1, 'Aspire to inspire before we expire.', 'Unknown', 'Steve', new Date(2018, 6, 4)),
+    new Quote(1, 'Everything you can imagine is real.', 'Pablo Picasso', 'Tom', new Date(2018, 6, 4))
+        ];
+
+
+        quoteDelete(isComplete, index) {
+    if (isComplete) {
+      const toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].author}`);
+
+      if ( toDelete) {
+        this.quotes.splice(index, 1 );
+      }
+    }
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
