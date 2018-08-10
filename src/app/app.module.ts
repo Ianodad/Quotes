62427d31de1cd281a8ac7db157e3aa5fcd781b5a
Ian Odhiambo <you@example.com>
2018-08-10 19:00:21 +0300

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { FormQuoteComponent } from './form-quote/form-quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
+    FormQuoteComponent,
     QuoteDetailsComponent
   ],
   imports: [
---
+++ src/app/form-quote/form-quote.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-form-quote',
+  templateUrl: './form-quote.component.html',
+  styleUrls: ['./form-quote.component.css']
+})
+export class FormQuoteComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
